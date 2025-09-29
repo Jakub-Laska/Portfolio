bfc5e671f6e616a5f2b826cefba57c8e16a72d7c
Jakub-Laska <laska.rayl@gmail.com>
2025-09-29 23:07:01 +0200

    add scroll animation

--- src/background.js
+++ src/background.js
@@ -1,10 +1,28 @@
 import './background.css';
 export function background(){
     const background = document.getElementById('background');
-    background.innerHTML = ` 
-  <div class="background-gradient-left"></div>
-  <div class="background-gradient-bot"></div>
-  <div class="background-gradient-right"></div>
+    background.innerHTML = `
+  <div class="background-gradient left"></div>
+  <div class="background-gradient bot"></div>
+  <div class="background-gradient right"></div>
   <div class="background-grain"></div>
   `;
+const gradients = document.querySelectorAll(".background-gradient");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  gradients.forEach((element, i) => {
+    const brightness = 1 + Math.sin(scrollY * 0.02 + i) * 0.08;
+    const saturate = 1 + Math.sin(scrollY * 0.016 + i) * 0.2;
+
+
+    element.style.filter = `
+      brightness(${brightness})
+      saturate(${saturate})
+      blur(36px)
+    `;
+  });
+});
+
 }
