407276ec33962df1b6d3bc97ff8011ce0651f8a4
Kuba Laska <laska.rayl@gmail.com>
2025-10-04 19:18:24 +0200

    Update hero.js

--- src/hero.js
+++ src/hero.js
@@ -3,7 +3,6 @@ export function initHero() {
     const hero = document.getElementById('hero');
     hero.innerHTML = `
     <div class="hero-img"></div>
-    <iframe class="spline" src='https://my.spline.design/starterscenecopy-D7MhSKbm89m4p74WE9BeGdDU/' frameborder='0'></iframe>
     <div class="hero-welcome-img"></div>
     `
 }
