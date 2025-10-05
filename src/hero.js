d29daff2144d26735a317b068f2cb50a8c4fbcfc
Jakub-Laska <laska.rayl@gmail.com>
2025-10-05 13:07:57 +0200

    sync

--- src/hero.js
+++ src/hero.js
@@ -3,7 +3,7 @@ export function initHero() {
     const hero = document.getElementById('hero');
     hero.innerHTML = `
     <div class="hero-img"></div>
-    <iframe class="spline" src='https://my.spline.design/starterscenecopy-D7MhSKbm89m4p74WE9BeGdDU/' frameborder='0'></iframe>
+    <iframe class="spline" src="https://my.spline.design/untitled-4PAG7B6is0PXcLJmRrjxaJDc-mc7/" frameborder="0" width="100%" height="100%"></iframe>
     <div class="hero-welcome-img"></div>
     `
 }
