634e08b53f7d5fbd3d12153f40559fb9f1b8cc97
Jakub-Laska <laska.rayl@gmail.com>
2025-10-12 20:56:14 +0200

    begin working on techstack

--- src/zindex.js
+++ src/zindex.js
@@ -3,11 +3,12 @@ import './variables.css';
 import { initBackground } from './background.js';
 import { initHeader } from './header.js';
 import { initHero } from './hero.js';
+import { initTechStack } from './techStack.js';
 
 function initAll(){
 initBackground();
 initHeader();
 initHero();
-
+initTechStack();
 }
 initAll();
