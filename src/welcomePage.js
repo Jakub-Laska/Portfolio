b2c6bf3d623591bad5e208516304b5cee02423ee
Jakub-Laska <laska.rayl@gmail.com>
2025-10-27 22:37:36 +0100

    feat: add welcome page

---
+++ src/welcomePage.js
@@ -0,0 +1,15 @@
+import "./welcomePage.css";
+
+export function initWelcomePage(){
+    const welcomePage = document.querySelector('#welcomePage');
+    welcomePage.innerHTML = `
+
+    <div>
+        <h1>welcome, glad to have you <span class="underscore">here.</span></h1>
+        <p>let me introduce myself - i'm jakub laska, an aspiring web developer.</p>
+        <p>'pixel perfect' is my motto, which i happen to ignore.</p><br>
+        <p>anyway... let me show you around.</p>
+    </div>
+
+    `
+}
--- src/zindex.js
+++ src/zindex.js
@@ -3,12 +3,14 @@ import './variables.css';
 import { initSplashScreen } from './splashScreen.js';
 import { initBackground } from './background.js';
 import { initHeader } from './header.js';
+import { initWelcomePage } from './welcomePage.js';
 import { initMain } from './main.js';
 import { initHybridScroll } from './hybridScroll.js';
 
 function initAll(){
 initSplashScreen()
 initBackground();
+initWelcomePage();
 initHeader();
 initMain();
 initHybridScroll();
