01f0b2caeb876a88c8823345c233bb77e2377ef6
Jakub-Laska <laska.rayl@gmail.com>
2025-10-28 22:55:03 +0100

    Update: add animations to the welcome text

--- src/welcomePage.js
+++ src/welcomePage.js
@@ -7,9 +7,13 @@ export function initWelcomePage(){
     <div>
         <h1>welcome, glad to have you <span class="underscore">here.</span></h1>
         <p>let me introduce myself - i'm jakub laska, an aspiring web developer.</p>
-        <p>'pixel perfect' is my motto, which i happen to ignore.</p><br>
-        <p>anyway... let me show you around.</p>
+        <p>'pixel perfect' is my motto, <span class="joke">which i happen to ignore.</span></p><br>
+        <p class="anyway">anyway... let me show you around.</p>
     </div>
 
     `
+    const joke = document.querySelector('.joke');
+    joke.classList.add('jokeAnimation');
+    const anyway = document.querySelector('.anyway');
+    anyway.classList.add('anywayAnimation');
 }
