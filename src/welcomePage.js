7f7b963ac66c9b3976c8ffe2373f1774d8bb2817
Jakub-Laska <laska.rayl@gmail.com>
2025-10-29 23:15:06 +0100

    Update: change slide animation for typing in welcomePage

--- src/welcomePage.js
+++ src/welcomePage.js
@@ -16,4 +16,50 @@ export function initWelcomePage(){
     joke.classList.add('jokeAnimation');
     const anyway = document.querySelector('.anyway');
     anyway.classList.add('anywayAnimation');
+
+
+animateJoke();
+animateAnyway();
+}
+    // typing animations
+function animateJoke() {
+    const joke = document.querySelector('.joke');
+  setTimeout(() => {
+    joke.style.opacity = '1';
+    const words = [
+      "which i happen to ignore...",
+    ];
+    let i = 0; // word index
+    let j = 0; // letter index
+    function type() {
+      const currentWord = words[i];
+        joke.textContent = currentWord.slice(0, j++);
+        if (j <= currentWord.length) {
+          setTimeout(type, 150); // type speed
+        }
+    }
+
+    type();
+  }, 3300); 
+}
+
+function animateAnyway() {
+    const anyway = document.querySelector('.anyway');
+  setTimeout(() => {
+    anyway.style.opacity = '1';
+    const words = [
+      "anyway... let me show you around.",
+    ];
+    let i = 0; // word index
+    let j = 0; // letter index
+    function type() {
+      const currentWord = words[i];
+        anyway.textContent = currentWord.slice(0, j++);
+        if (j <= currentWord.length) {
+          setTimeout(type, 150); // type speed
+        }
+    }
+
+    type();
+  }, 7350); 
 }
