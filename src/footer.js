39344d88ea60522a818755532ca8bbf8af46823e
Jakub-Laska <laska.rayl@gmail.com>
2025-11-02 19:46:51 +0100

    Feat: add new main logo design

--- src/footer.js
+++ src/footer.js
@@ -2,5 +2,10 @@ import "./footer.css";
 
 export function initFooter() {
     const footer = document.getElementById('footer');
-    footer.innerHTML = 'lorem';
+    footer.innerHTML = `
+    <p>lorem</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, at possimus incidunt commodi voluptas doloribus
+    voluptatibus quidem dignissimos esse, sequi quis. Necessitatibus id cum provident est, sapiente ipsa voluptas accusamus?</p>
+    <p class="name">jakub laska<span>.</span></p>
+    `;
 }
