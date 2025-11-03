a5a224c749ce4b8723f921f539755529eb6a5dd5
Jakub-Laska <laska.rayl@gmail.com>
2025-11-03 19:00:42 +0100

    Update: add header and signature to the footer

--- src/footer.js
+++ src/footer.js
@@ -3,9 +3,12 @@ import "./footer.css";
 export function initFooter() {
     const footer = document.getElementById('footer');
     footer.innerHTML = `
-    <p>lorem</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, at possimus incidunt commodi voluptas doloribus
-    voluptatibus quidem dignissimos esse, sequi quis. Necessitatibus id cum provident est, sapiente ipsa voluptas accusamus?</p>
-    <p class="name">jakub laska<span>.</span></p>
+    <h1>Let's work togheter.</h1>
+    <div class="signature">
+        <p>Designed and Developed by Jakub Laska</p>
+        <p>52º 10' 52.793" N 21º 1' 36.192" E</p>
+        <p>Since 2025</p>
+    </div>
+    <p class="logo">jakub laska<span>.</span></p>
     `;
 }
