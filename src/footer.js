f96c10ede0fe8aa70a92ffd1c134fbb55d962b03
Jakub-Laska <laska.rayl@gmail.com>
2025-11-04 22:37:28 +0100

    Feat: add randome quote section to the footer, fix z-index issue

--- src/footer.js
+++ src/footer.js
@@ -5,10 +5,16 @@ export function initFooter() {
     footer.innerHTML = `
     <h1>Let's work togheter.</h1>
     <div class="signature">
-        <p>Designed and Developed by Jakub Laska</p>
-        <p>52º 10' 52.793" N 21º 1' 36.192" E</p>
-        <p>Since 2025</p>
+        <p>Designed and Developed by Jakub Laska.</p>
+        <p>52º 10' 52.793" N 21º 1' 36.192" E.</p>
+        <p>Since 2025.</p>
     </div>
     <p class="logo">jakub laska<span>.</span></p>
+    <div class="quote">
+        <div>
+            Something to reflect on.
+            <button>random quote.</button>
+        </div>
+    </div>
     `;
 }
